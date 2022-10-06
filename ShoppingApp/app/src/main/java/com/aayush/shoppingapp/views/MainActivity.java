199ccd6087c9ae8ddc83821f09c07b3bb6b3066d
Aayush Puranik <aayushpuranik1992@gmail.com>
2022-10-06 20:25:32 +0530

    commitng initial commit

---
+++ ShoppingApp/app/src/main/java/com/aayush/shoppingapp/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aayush.shoppingapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aayush.shoppingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
