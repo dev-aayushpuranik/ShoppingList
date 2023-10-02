4c247120eda5a27bbccbc17ebe80b2560bbe2f9d
Aayush Puranik <aayushpuranik1992@gmail.com>
2023-10-02 09:56:20 +0530

    added collapse and expand in UIHelper

--- ShoppingApp/app/src/main/java/com/aayush/shoppingapp/common/helper/UIHelper.kt
+++ ShoppingApp/app/src/main/java/com/aayush/shoppingapp/common/helper/UIHelper.kt
@@ -1,8 +1,11 @@
 package com.aayush.shoppingapp.common.helper
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 class UIHelper {
@@ -16,7 +19,35 @@ class UIHelper {
             Snackbar.make(view, message, Snackbar.LENGTH_LONG)
                 .setAction(actionText) {
                     onAction.invoke()
-                }.show()
+                }
+                .setDuration(3000)
+                .show()
+        }
+
+        fun expand(v: View, duration: Int, targetHeight: Int) {
+            val prevHeight = v.height
+            v.visibility = View.VISIBLE
+            val valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight)
+            valueAnimator.addUpdateListener { animation ->
+                v.layoutParams.height = animation.animatedValue as Int
+                v.requestLayout()
+            }
+            valueAnimator.interpolator = DecelerateInterpolator()
+            valueAnimator.duration = duration.toLong()
+            valueAnimator.start()
+        }
+
+        fun collapse(v: View, duration: Int, targetHeight: Int) {
+            val prevHeight = v.height
+            val valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight)
+            valueAnimator.interpolator = DecelerateInterpolator()
+            valueAnimator.addUpdateListener { animation ->
+                v.layoutParams.height = animation.animatedValue as Int
+                v.requestLayout()
+            }
+            valueAnimator.interpolator = DecelerateInterpolator()
+            valueAnimator.duration = duration.toLong()
+            valueAnimator.start()
         }
     }
 }
