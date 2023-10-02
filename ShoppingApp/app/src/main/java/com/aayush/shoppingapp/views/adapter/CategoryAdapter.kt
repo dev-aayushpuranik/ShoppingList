805dc4a1d0a225adda8f2ed1abe764a9c7c17793
Aayush Puranik <aayushpuranik1992@gmail.com>
2023-10-02 10:25:53 +0530

    added config changes and added colors manually for CategoryAdapter.kt

--- ShoppingApp/app/src/main/java/com/aayush/shoppingapp/views/adapter/CategoryAdapter.kt
+++ ShoppingApp/app/src/main/java/com/aayush/shoppingapp/views/adapter/CategoryAdapter.kt
@@ -1,13 +1,15 @@
 package com.aayush.shoppingapp.views.adapter
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.aayush.shoppingapp.R
 import com.aayush.shoppingapp.common.extensions.SetViewVisible
-import com.aayush.shoppingapp.common.extensions.orDefaut
+import com.aayush.shoppingapp.common.extensions.orDefault
 import com.aayush.shoppingapp.databinding.CategoryRowViewBinding
 import com.aayush.shoppingapp.models.CategoryModel
 
@@ -23,7 +25,7 @@ class CategoryAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, type: Int): CategoryViewHolder {
         val binding = CategoryRowViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CategoryViewHolder(binding) { index ->
+        return CategoryViewHolder(parent.context, binding) { index ->
             onItemClick(data[index])
         }
     }
@@ -33,11 +35,11 @@ class CategoryAdapter(
     }
 
     override fun getItemCount(): Int {
-        return data.size.orDefaut()
+        return data.size.orDefault()
     }
 }
 
-class CategoryViewHolder(private val binding: CategoryRowViewBinding, onItemClick: (Int) -> Unit):
+class CategoryViewHolder(private val context: Context, private val binding: CategoryRowViewBinding, onItemClick: (Int) -> Unit):
     RecyclerView.ViewHolder(binding.root) {
     init {
         itemView.setOnClickListener {
@@ -46,8 +48,24 @@ class CategoryViewHolder(private val binding: CategoryRowViewBinding, onItemClic
     }
 
     fun bind(model: CategoryModel) {
+        if(model.CategoryId == 0L) {
+            binding.imageView2.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.important_icon))
+            binding.imageView2.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.impotant_color))
+        } else {
+            binding.imageView2.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_baseline_format_list_bulleted_24))
+            binding.imageView2.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.app_text_color))
+        }
         binding.categoryNameTV.text = model.CategoryName
         binding.DiscriptionNameTV.text = model.Description
         binding.DiscriptionNameTV.SetViewVisible(model.Description.isNotEmpty())
+
+        val value = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10F, context.resources.displayMetrics)
+        binding.cardView.radius = value
+        binding.cardView.preventCornerOverlap = true
+        binding.cardView.useCompatPadding = true
+
+        binding.cardView.background = ContextCompat.getDrawable(context, R.drawable.rounded_corners)
+        binding.categoryNameTV.setTextColor(ContextCompat.getColor(context, R.color.app_text_color))
+        binding.DiscriptionNameTV.setTextColor(ContextCompat.getColor(context, R.color.app_text_color))
     }
 }
